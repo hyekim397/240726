f4252749c343bea533361ee15dc0c8e967b5f7b4
hyekim397 <inekim.et@gmail.com>
2024-07-26 15:53:19 +0900

    Update 기온데이터시각화.py

--- pages/기온데이터시각화.py
+++ pages/기온데이터시각화.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
 # Load and clean the data
 file_path = 'daily_temp.csv'
@@ -19,8 +20,13 @@ yearly_stats = data.groupby('연도').agg({
 
 yearly_stats.rename(columns={'평균기온(℃)': '평균기온', '최저기온(℃)': '최저기온', '최고기온(℃)': '최고기온'}, inplace=True)
 
+# Configure the font to support Korean characters
+font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'  # Adjust the path based on your system
+font_prop = fm.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = font_prop.get_name()
+
 # Streamlit app
-st.title('연도별 평균, 최저, 최고 기온 변화 추이')
+st.title('기온 데이터 시각화')
 
 # User selection for graph type
 graph_type = st.selectbox('그래프 유형을 선택하세요', ('꺾은선 그래프', '막대 그래프'))
@@ -33,7 +39,7 @@ if graph_type == '꺾은선 그래프':
     ax.plot(yearly_stats['연도'], yearly_stats['최고기온'], label='최고기온')
     ax.set_xlabel('연도')
     ax.set_ylabel('기온 (℃)')
-    ax.set_title('연도별 평균, 최저, 최고 기온 꺾은선 그래프')
+    ax.set_title('기온 데이터 시각화')
     ax.legend()
     st.pyplot(fig)
 elif graph_type == '막대 그래프':
@@ -48,6 +54,6 @@ elif graph_type == '막대 그래프':
 
     ax.set_xlabel('연도')
     ax.set_ylabel('기온 (℃)')
-    ax.set_title('연도별 평균, 최저, 최고 기온 막대 그래프')
+    ax.set_title('기온 데이터 시각화')
     ax.legend()
     st.pyplot(fig)
